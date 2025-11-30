402da2c88c37dc930f8e6f976c60bb2f1260e558
Thong Tran <thong.tran@gatech.edu>
2025-11-29 19:48:15 -0700

    changed in parameters to use slice instead of new object

--- src/aoc_2024/day_1.rs
+++ src/aoc_2024/day_1.rs
@@ -1,5 +1,5 @@
 // this is my quick and dirty solution to day 1 from 2024 advent of code
-fn day_one_2024(l1: &mut Vec<i32>, l2: &mut Vec<i32>) -> i32 {
+fn day_one(l1: &mut [i32], l2: &mut [i32]) -> i32 {
     l1.sort();
     l2.sort();
     let mut total_distance = 0;
@@ -12,7 +12,7 @@ fn day_one_2024(l1: &mut Vec<i32>, l2: &mut Vec<i32>) -> i32 {
 }
 
 // cleaner solution
-fn day_one_2024_no_sort(l1: &mut Vec<i32>, l2: &mut Vec<i32>) -> i32 {
+fn day_one_no_sort(l1: &mut [i32], l2: &mut [i32]) -> i32 {
     let l1_sum: i32 = l1.iter().sum();
     let l2_sum: i32 = l2.iter().sum();
 
@@ -24,23 +24,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_2024_day_one() {
+    fn test_day_one() {
         let mut l1 = vec![3, 4, 2, 1, 3, 3];
         let mut l2 = vec![4, 3, 5, 3, 9, 3];
         let expected_result = 11;
 
-        let result = day_one_2024(&mut l1, &mut l2);
+        let result = day_one(&mut l1, &mut l2);
 
         assert_eq!(result, expected_result);
     }
 
     #[test]
-    fn test_2024_day_one_a() {
+    fn test_day_one_no_sort() {
         let mut l1 = vec![3, 4, 2, 1, 3, 3];
         let mut l2 = vec![4, 3, 5, 3, 9, 3];
         let expected_result = 11;
 
-        let result = day_one_2024_no_sort(&mut l1, &mut l2);
+        let result = day_one_no_sort(&mut l1, &mut l2);
 
         assert_eq!(result, expected_result);
     }
